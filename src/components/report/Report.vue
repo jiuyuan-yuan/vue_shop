95eca0b8b3b0b05c43fdec0002063cb397db419e
benc <2627898178@qq.com>
2020-06-21 12:21:52 +0800

    完成报表

---
+++ src/components/report/Report.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card>
+      <div id="main"
+           style="width: 750px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  data () {
+    return {
+      // 指定图表的配置项和数据
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  created () { },
+  async mounted () {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取列表失败！')
+    }
+    // console.log(res.data)
+    const result = _.merge(this.options, res.data)
+    // 使用刚指定的配置项和数据显示图表。
+    console.log(result)
+    myChart.setOption(result)
+  },
+  methods: {}
+
+}
+</script>
+
+<style lang="less" scoped>
+</style>
